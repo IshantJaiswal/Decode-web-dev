e7ce320f7f1977834833723605ff7fc666399b9b
IshantJaiswal <117654993+IshantJaiswal@users.noreply.github.com>
2024-01-27 16:53:00 +0530

    Add files via upload

---
+++ Operator.js
@@ -0,0 +1,58 @@
+/* var a = 10
+var a = 20
+console.log(a)
+*/
+
+
+/** let a = 10
+let b = 'lakshay'
+console.log( a, b )
+*/
+
+ /** const a = 10
+ b = 20
+ c = b - a 
+ d = "Hello \n World" // \n is used for new line in string
+ console.log( a + b, c )
+//  console.log (c)
+console.log (d)
+*/
+
+
+
+
+// Arithmetic operator
+/**  
+num1 = 20
+num2 = 2
+
+/*
+var sum = num1 + num2
+console.log(sum)
+
+var sub = num1 - num2
+console.log(sub)
+
+var mul = num1 * num2
+console.log(mul)
+
+var div = num1 / num2
+console.log(div)
+
+var remainder = num1 % num2
+console.log(remainder)
+*/
+
+//--------------------------------------------------
+
+// details about incriment & decriment operator
+
+// i = 5
+/*
+i++  // i = i+1  
+++i // i = 1+ i 
+// i-- // i = i-1
+--i // i = 1-i
+console.log(i)
+*/
+
