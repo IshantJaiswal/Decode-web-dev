ead8ef94219ebc1913ef44af28117f268e0b60ae
IshantJaiswal <117654993+IshantJaiswal@users.noreply.github.com>
2024-01-30 14:22:27 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,50 @@
+/* function me(a,b){
+   a=20
+   b=10
+    if(a!=b){
+        console.log("A is Bigger")
+    }
+    else if (a=b){
+        console.log("Both are Equal")
+    }
+    else{
+        console.log("B is Bigger")
+    }
+
+}
+me() 
+me()
+me()
+*/
+
+
+/* a=10
+ function sum(a,b){
+    return a+b
+    
+}
+console.log(sum(a,6)) */
+
+
+
+
+
+
+//console.log("Namaste Vanshika !")
+
+function Vanshika(){
+    const kavish = 8
+    const vanshikajaiswal = 9
+      if(kavish>vanshikajaiswal){
+          console.log("Yee Kavish is older" (kavish))
+      }
+      else if (kavish==vanshikajaiswal){
+          console.log("Both are Equal age"(kavish, vanshikajaiswal))
+      }
+      else{
+          console.log("Ohhh Vanshika is older")
+      }
+      console.log("Vedic is same as Vanshika")
+      console.log("age of Vedic is",vanshikajaiswal);
+  }
+  Vanshika()
