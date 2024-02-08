9d92f6243e9c3014050376d67147e5ceccc8dea9
IshantJaiswal <117654993+IshantJaiswal@users.noreply.github.com>
2024-02-08 23:37:04 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,94 @@
+/*
+a=30
+b=30
+if(a<b){
+    console.log('A is smaller than B')
+}
+else if(a==b){
+    console.log('A & B are equal')
+}
+else{
+    console.log('A is bigger than B')
+}
+*/
+
+/*
+function naira(){
+    for(i=0;i<5;i++){
+    console.log("Hello Naira")
+}}
+naira() // 5 times print naira
+naira() //5 times print naira
+naira()
+*/
+
+/*
+a=10 // shahi paneer
+ function sum(a,b,c){  // b & c is our gulab jamun & ice-cream
+    return a+b+c // wapis aate time sab cheej le aoge
+}
+console.log(sum(a,6,4)) 
+
+*/
+
+
+
+
+/*
+console.log("Namaste Vanshika !")
+
+function Vanshika(){
+   const kavish=8
+   const vanshikajaiswal=9
+     if(kavish>vanshikajaiswal){
+         console.log("Yee Kavish is older" (kavish))
+     }
+     else if (kavish==vanshikajaiswal){
+         console.log("Both are Equal age"(kavish, vanshikajaiswal))
+     }
+     else{
+         console.log("Ohhh Vanshika is older")
+     }
+     console.log("Vedic is same as Vanshika")
+     console.log("age of Vedic is",vanshikajaiswal);
+ }
+ Vanshika()
+*/
+
+
+
+/*
+console.log(" Hey Naira & Manas ")
+function Age(){
+    Naira = 25
+    Manas = 32
+    if(Naira<Manas){
+        console.log(' Manas is Older than Naira ','age is',Manas)
+    } else if(Naira == Manas){
+        console.log(" Naira & Manas both are equal ",'age is',Manas,Naira)
+    }
+    else{
+        console.log(" Naira is Older","age is",Naira)
+    }
+}
+Age()
+*/
+/*
+for(i=2;i<8;i++){
+    console.log(i)
+}
+*/
+/*
+for(i=0,b=5; i<10,b<10; i++,b++){
+    console.log('value of i & b' , i, " , " ,b)
+}*/
+
+/*
+for(i=0; i<10; i++){
+    console.log('value of i ' , i)
+}
+
+for(b=1; b>-10; b--){
+    console.log('value of  b' ,b)
+}
+*/
