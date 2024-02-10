7f8d55525944b00b38a1aa38e8d90b51e6adfc5c
IshantJaiswal <117654993+IshantJaiswal@users.noreply.github.com>
2024-02-10 23:28:44 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,45 @@
+/* arr = [ 1 , 2 , 3 , 4 , 5 ]
+// console.log(arr)
+console.log(arr.pop())
+// console.log(arr)
+
+console.log(arr.push(7))
+console.log(arr) */
+/*
+fruits = ["Mango" , "Apple" , 'Papaya']
+console.log(fruits.pop())
+
+fruits.push("WaterMelon" , "Grapes" , "Bannana")
+console.log(fruits)
+*/
+/*
+arr = [ 1 , 2 , 3 , 4 , 5 ]
+
+x = arr.shift()
+// console.log(x)
+// console.log(arr)
+
+arr.unshift(51)
+console.log(arr) */
+/*
+arr1 = [3,4,5,6]
+arr2 = [7,8,9,0]
+
+arr = arr1.concat(arr2)
+console.log(arr)
+console.log(arr.length)
+
+arr_new = arr1 + arr2 // creates new string
+console.log(typeof arr_new)
+console.log(arr_new)
+*/
+
+let fruits = [ "apple" , "banana", "carrot"]
+let veges = [ "baingan" , "karela" , "aalo"]
+let result = fruits.join(veges)
+console.log(result)
+console.log(typeof result)
+
+
+// let result2 = fruits.join("-")
+// console.log(result2)
