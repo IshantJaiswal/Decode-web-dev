93def37d77a72946764ab692dd53afad155e692d
IshantJaiswal <117654993+IshantJaiswal@users.noreply.github.com>
2024-03-07 11:17:49 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,35 @@
+/*const person = {
+    name: "naira",
+    age : 21,
+    city: "Ghaziabad"
+}
+console.log(person)
+
+const user = {
+    1: "Naira",
+    2: "Manas",
+    3: 'Saurav'
+}
+console.log(typeof user)
+console.log(user)
+*/
+const person={
+    name : "manas",
+    age :  80,
+    greet: function(){
+        console.log("Hello friends")
+    }
+}
+
+/*
+const obj = new Object()
+console.log(obj)
+*/
+/*
+function emp(id,name,salary){
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+}
+const emp_obj = new emp(1,"Rahul", 50000);
+console.log(emp_obj);*/
